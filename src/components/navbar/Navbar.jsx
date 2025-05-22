ab45b6039150540e51f601383fbc5f10a29d43b6
MinalKinage7 <119405298+MinalKinage7@users.noreply.github.com>
2025-05-22 08:47:41 +0530

    Initial commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import './navbar.scss';
+function Navbar(){
+    return (
+      <>
+        <nav>
+          <div className="left">
+            <a href="" className='logo'>
+            <img src="/src/assets/logo.png" alt="" />
+            <span>RealEstate</span>
+            </a>
+            <a href="">Home</a>
+            <a href="">Contact</a>
+            <a href="">Others</a>
+          </div>
+          <div className="right">
+          <a href="">Sign In</a>
+          <a href="">Sign Up</a>
+          <div className="menuIcon">
+          <img src="/src/assets/menu.png" alt="" />
+          </div>
+          </div>
+        </nav>
+      </>
+    );
+}
+
+export default Navbar;
